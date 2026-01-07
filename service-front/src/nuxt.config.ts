292cc05ebbccd288748e32ca81a1f2db02886d31
Jeff Silva <jeferson.i.silva@gmail.com>
2026-01-07 13:43:39 -0300

    feat: Implement new Nuxt.js frontend service with Docker Compose configuration and development scripts.

---
+++ service-front/src/nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  ssr: false,
+  modules: [
+    ['@nuxt/icon', {}],
+    ['@nuxt/scripts', {}],
+  ],
+
+  app: {
+    head: {
+      script: [{ src: "https://cdn.tailwindcss.com" }],
+    },
+  },
+});
